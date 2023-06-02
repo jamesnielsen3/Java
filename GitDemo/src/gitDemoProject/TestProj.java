29be2552b71d057d2b3bda5f602cb451a521505e
Optimus II <Optimus II@Optimus-Desktop>
2023-06-02 15:26:27 -0500

    Added in for loop with numbered message

--- GitDemo/src/gitDemoProject/TestProj.java
+++ GitDemo/src/gitDemoProject/TestProj.java
@@ -1,10 +1,16 @@
 package gitDemoProject;
 
+
 public class TestProj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for(int i=0; i<5; i++){
+			System.out.println("Counting: " + i);
+
+		}
+		
 	}
 
 }
