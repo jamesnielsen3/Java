feaa6a42ad39c6aa8b93ef3ea2518e19844581cf
Optimus II <Optimus II@Optimus-Desktop>
2023-06-05 09:51:33 -0500

    Added database interactions to update qty and remove

--- RasCOP3805CSPRT2/module_2/Project.java
+++ RasCOP3805CSPRT2/module_2/Project.java
@@ -17,17 +17,17 @@ public class Project {
 		
 		//Main menu for the user
 		
-		System.out.println("Welcome to the Inventory Management System");
+		//System.out.println("Welcome to the Inventory Management System");
 		System.out.println("Please select one of the following options:");
-		System.out.println("----------------------------");
+		System.out.println("------------------------------");
 		System.out.println("1: Add an item");
-		System.out.println("2: Update an item");
-		System.out.println("3: Delete an item");
-		System.out.println("4: Update inventory quantity");
-		System.out.println("5: Search for an item by name");
-		System.out.println("6: View all items");
+		System.out.println("2: Update item quantity");
+		System.out.println("3: Remove an item");
+		//System.out.println("4: Update inventory quantity");
+		System.out.println("4: Search for an item by name");
+		System.out.println("5: View all items");
 		System.out.println("q: Quit the program");
-		System.out.println("----------------------------");
+		System.out.println("------------------------------");
 
 		
 				
@@ -38,7 +38,9 @@ public class Project {
 			
 			if (input == '1') {
 				//call the add item method
-				System.out.println("Okay. Let's add an item.");
+				System.out.println("---------------------------------");
+				System.out.println("You have selected to add an item.");
+				System.out.println("---------------------------------");
 				System.out.println("Enter the product name: ");
 				String pName = scanner.next();
 				System.out.println("Enter the product quantity: ");
@@ -49,24 +51,40 @@ public class Project {
 			} 
 			else if (input == '2') {
 				//call update item method
-				System.out.println("Okay. Lets update an item");
+				System.out.println("------------------------------------------");
+				System.out.println("You have selected to update item quantity.");
+				System.out.println("------------------------------------------");
+
+				System.out.println("Enter the product ID number: ");
+				String pID = scanner.next();
+				System.out.println("Enter the new product quantity: ");
+				int qty = scanner.nextInt();
+				
+				updateQty(pID, qty);
 			}
 			else if (input == '3') {
 				//call delete item method
-				System.out.println("Okay. Lets delete an item");
+				System.out.println("------------------------------------");
+				System.out.println("You have selected to remove an item.");
+				System.out.println("------------------------------------");
+				
+				System.out.println("Enter the product ID number: ");
+				String pID = scanner.next();
+				
+				removeItem(pID);
 			}
-			else if (input == '4') {
+			//else if (input == '4') {
 				//call update quantity method
-				System.out.println("Okay. Lets update a quantity");
-			}
-			else if (input == '5') {
+			//	System.out.println("Okay. Lets update a quantity");
+			//}
+			else if (input == '4') {
 				//call search method
 				System.out.println("Okay. Lets search for an item");
 				System.out.println("Enter the product name: ");
 				String pName = scanner.next();
 				SearchProduct(pName);
 			}
-			else if (input == '6') {
+			else if (input == '5') {
 				//call search method
 				System.out.println("Okay. Lets view all items");
 				GetProducts();
@@ -114,11 +132,64 @@ public class Project {
 			
 			ps.execute();
 			connect.close();
-			//ResultSet rs = ps.executeQuery();
+			System.out.println("Item: "+ productName + " has been added with a quantity of " + qty );
 			
-			//while(rs.next()) {
-				//System.out.println(rs.getString("ProductID") + " - " + rs.getString("productName"));
-			//}
+		} catch (SQLException e) {
+		
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	public void updateQty(String pID, int qty) {
+		try {
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/InventorySystem","root","Database123!");
+			
+			
+			
+			PreparedStatement ps = connect.prepareStatement("UPDATE products SET qty = '" + qty + "' WHERE productID = '" + pID +"';");
+			
+			ps.execute();
+			connect.close();
+			System.out.println("Item # "+ pID + " has been updated to a quantity of: " + qty );
+			
+		} catch (SQLException e) {
+		
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/InventorySystem","root","Database123!");
+			
+			PreparedStatement ps = connect.prepareStatement("Select * from Products WHERE productID = '" + pID +"';");
+			
+			ResultSet rs = ps.executeQuery();
+			
+			
+			while(rs.next()) {
+				System.out.println("Current Product Details:");
+				System.out.println(rs.getString("ProductID") + " - " + rs.getString("productName") + " - " + rs.getString("qty"));
+			}
+			connect.close();
+			//get one database interaction working
+		} catch (SQLException e) {
+		
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	public void removeItem(String pID) {
+		try {
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/InventorySystem","root","Database123!");
+			
+			
+			
+			PreparedStatement ps = connect.prepareStatement("DELETE from products where productID = '" + pID + "';");
+			
+			ps.execute();
+			connect.close();
+			System.out.println("Item # " + pID + " has been removed from table Products");
 			
 		} catch (SQLException e) {
 		
